81e8db8ab3a74b4424e2f3b2f0721481bef3aad6
chdxia <chdxiamail@gmail.com>
2023-09-09 23:27:15 +0800

    freesql声明

--- src/LRtest.Api/Program.cs
+++ src/LRtest.Api/Program.cs
@@ -1,5 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
+Func<IServiceProvider, IFreeSql> fsqlFactory = r =>
+{
+    IFreeSql fsql = new FreeSql.FreeSqlBuilder()
+        .UseConnectionString(FreeSql.DataType.MySql, @"Server=chdxia.com;Database=lrtest;Uid=root;Pwd=******;")
+        .UseMonitorCommand(cmd => Console.WriteLine($"Sql：{cmd.CommandText}"))//监听SQL语句
+        .UseAutoSyncStructure(true) //自动同步实体结构到数据库，FreeSql不会扫描程序集，只有CRUD时才会生成表。
+        .Build();
+    return fsql;
+};
+
+builder.Services.AddSingleton<IFreeSql>(fsqlFactory);
+
 // Add services to the container.
 
 builder.Services.AddControllers();
