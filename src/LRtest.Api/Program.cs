9d2bbd72a67e976be182dac9c017813e71c934b7
chdxia <chdxiamail@gmail.com>
2023-09-12 20:47:24 +0800

    调整依赖注入

--- src/LRtest.Api/Program.cs
+++ src/LRtest.Api/Program.cs
@@ -3,27 +3,32 @@ var builder = WebApplication.CreateBuilder(args);
 Func<IServiceProvider, IFreeSql> fsqlFactory = r =>
 {
     IFreeSql fsql = new FreeSql.FreeSqlBuilder()
-        .UseConnectionString(FreeSql.DataType.MySql, @"Server=chdxia.com;Database=lrtest;Uid=root;Pwd=******;")
+        .UseConnectionString(FreeSql.DataType.MySql, @"Server=chdxia.com;Port=3306;Database=lrtest;Uid=root;Pwd=******;")
         .UseMonitorCommand(cmd => Console.WriteLine($"Sql：{cmd.CommandText}"))//监听SQL语句
         .UseAutoSyncStructure(true) //自动同步实体结构到数据库，FreeSql不会扫描程序集，只有CRUD时才会生成表。
         .Build();
     return fsql;
 };
 
-builder.Services.AddSingleton<IFreeSql>(fsqlFactory);
-
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
-    // 启用 XML 注释
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "LRtest.Api.xml"));
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "LRtest.Api.xml"));// 启用 XML 注释
 });
+builder.Services.AddSingleton(fsqlFactory);
+builder.Services.AddSingleton<IUserRepository, UserRepository>();
 
 var app = builder.Build();
+
+/*
+using (IServiceScope serviceScope = app.Services.CreateScope())
+{
+    var fsql = serviceScope.ServiceProvider.GetRequiredService<IFreeSql>();
+    fsql.CodeFirst.SyncStructure(typeof(LRtest.Domain.User));//Topic 为要同步的实体类
+}
+*/
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
