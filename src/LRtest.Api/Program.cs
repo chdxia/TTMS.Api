ca8b56a912e82ccb57da7aa1a419fdbf3d0a75f9
chdxia <chdxiamail@gmail.com>
2023-09-12 21:35:55 +0800

    mysql改用postgresql

--- src/LRtest.Api/Program.cs
+++ src/LRtest.Api/Program.cs
@@ -3,7 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 Func<IServiceProvider, IFreeSql> fsqlFactory = r =>
 {
     IFreeSql fsql = new FreeSql.FreeSqlBuilder()
-        .UseConnectionString(FreeSql.DataType.MySql, @"Server=chdxia.com;Port=3306;Database=lrtest;Uid=root;Pwd=******;")
+        .UseConnectionString(FreeSql.DataType.PostgreSQL, @"Host=chdxia.com;Port=5432;Database=lrtest;Username=postgres;Password=******;")
         .UseMonitorCommand(cmd => Console.WriteLine($"Sql：{cmd.CommandText}"))//监听SQL语句
         .UseAutoSyncStructure(true) //自动同步实体结构到数据库，FreeSql不会扫描程序集，只有CRUD时才会生成表。
         .Build();
