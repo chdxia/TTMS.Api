1e967ec39f0239189f19a7b6239b64eb304b4a28
chdxia <chdxiamail@gmail.com>
2023-09-13 22:30:14 +0800

    隐藏配置文件

--- src/LRtest.Api/Program.cs
+++ src/LRtest.Api/Program.cs
@@ -1,9 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.Dev.json")
+    .Build();
+
 Func<IServiceProvider, IFreeSql> fsqlFactory = r =>
 {
     IFreeSql fsql = new FreeSql.FreeSqlBuilder()
-        .UseConnectionString(FreeSql.DataType.PostgreSQL, @"Host=chdxia.com;Port=5432;Database=lrtest;Username=postgres;Password=******;")
+        .UseConnectionString(FreeSql.DataType.PostgreSQL, @config.GetSection("DBConnectionStrings")["DefaultConnection"])
         .UseMonitorCommand(cmd => Console.WriteLine($"Sql：{cmd.CommandText}"))//监听SQL语句
         .UseAutoSyncStructure(true) //自动同步实体结构到数据库，FreeSql不会扫描程序集，只有CRUD时才会生成表。
         .Build();
