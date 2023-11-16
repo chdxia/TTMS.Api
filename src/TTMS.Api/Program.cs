7fbe5cfe358a587230c2b0e637f4cda7ee9edad0
chdxia <chdxiamail@gmail.com>
2023-11-16 23:20:06 +0800

    refactor: 优化Swagger生成器的配置，优化Freesql实例的创建和配置

--- src/TTMS.Api/Program.cs
+++ src/TTMS.Api/Program.cs
@@ -9,36 +9,17 @@ var config = new ConfigurationBuilder()
 
 
 // 注册服务
-builder.Services.AddControllers(options =>
-{
-    options.Filters.Add(typeof(ValidateModelAttribute)); // 注册自定义全局过滤器
-}).AddDataAnnotationsLocalization();
+builder.Services.AddControllers(options =>{options.Filters.Add(typeof(ValidateModelAttribute));}).AddDataAnnotationsLocalization(); // 注册自定义全局过滤器
+
+builder.Services.AddScoped<ValidateModelAttribute>();
 
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSwaggerGen(options =>
-{
-    options.DocInclusionPredicate((docName, apiDesc) =>
-    {
-        return true; // 包含所有控制器和操作方法，根据需要修改左面的逻辑来包含或排除特定的控制器和操作方法
-    });
-    options.DocumentFilter<EnumDocumentFilter>(); // 显示枚举值;枚举属性;枚举描述
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "TTMS.Api.xml")); // 启用 XML 注释
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "TTMS.DTO.xml"));
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "TTMS.Enums.xml"));
-});
-
-builder.Services.AddSingleton(r =>
-{
-    IFreeSql fsql = new FreeSql.FreeSqlBuilder()
-        .UseConnectionString(FreeSql.DataType.PostgreSQL, config.GetConnectionString("DefaultDBConnection"))
-        .UseMonitorCommand(cmd => Console.WriteLine($"Sql：{cmd.CommandText}")) // 监听SQL语句
-        .UseAutoSyncStructure(true) // 自动同步实体结构到数据库，FreeSql不会扫描程序集，只有CRUD时才会生成表
-        .Build();
-    return fsql;
-});
+builder.Services.AddSwaggerGen(SwaggerProvider.ConfigureSwaggerGen); // Swagger配置
 
 builder.Services.AddAutoMapper(typeof(TTMS.DTO.Mapper.UserMapper).Assembly); // 注册映射规则
+
+builder.Services.AddSingleton(r => FreeSqlProvider.CreateFreeSqlInstance(config)); // FreeSql实例
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IGroupRepository, GroupRepository>();
