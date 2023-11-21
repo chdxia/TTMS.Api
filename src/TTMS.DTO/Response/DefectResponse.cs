ff78c9f3b476d3207c56cc63b9902004441b3286
chdxia <chdxiamail@gmail.com>
2023-11-22 00:09:02 +0800

    feat: DefectResponse

--- src/TTMS.DTO/Response/DefectResponse.cs
+++ src/TTMS.DTO/Response/DefectResponse.cs
@@ -5,5 +5,54 @@
     /// </summary>
     public class DefectResponse
     {
+        /// <summary>
+        /// 缺陷id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 需求id
+        /// </summary>
+        public int DemandId { get; set; }
+
+        /// <summary>
+        /// 缺陷标题
+        /// </summary>
+        public string? Title { get; set; }
+
+        /// <summary>
+        /// 缺陷描述
+        /// </summary>
+        public string? Description { get; set; }
+
+        /// <summary>
+        /// 严重程度
+        /// </summary>
+        public DefectType DefectType { get; set; }
+
+        /// <summary>
+        /// 缺陷状态
+        /// </summary>
+        public DefectState DefectState { get; set; }
+
+        /// <summary>
+        /// 创建人
+        /// </summary>
+        public int CreateBy { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime? CreateTime { get; set; }
+
+        /// <summary>
+        /// 最后更新人
+        /// </summary>
+        public int UpdateBy { get; set; }
+
+        /// <summary>
+        /// 最后更新时间
+        /// </summary>
+        public DateTime? UpdateTime { get; set; }
     }
 }
